616348d3f6b63130f618bd03abf969458c7eda98
rikii <rhikisulistiyo@gmail.com>
2025-05-08 09:21:27 +0700

    Initial commit

---
+++ src/main/java/com/mycompany/p_uts_23090115_c_2025/CRUD_23090115_C_2025.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.p_uts_23090115_c_2025;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class CRUD_23090115_C_2025 {
+    
+}
---
+++ src/main/java/com/mycompany/p_uts_23090115_c_2025/P_UTS_23090115_C_2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p_uts_23090115_c_2025;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class P_UTS_23090115_C_2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
